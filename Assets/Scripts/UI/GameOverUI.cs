b31ac63f11f0100b2ce264996afbd9b917198d0a
Ryan Carpenter <dev.ryan1184@gmail.com>
2023-08-03 21:16:39 -0700

    Small Tweaks

---
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private GameObject leaderboardUI;
+
+    private void Start()
+    {
+        // Making sure the leaderboard menu is not accidently set active when we load the scene
+        if (leaderboardUI.activeSelf)
+            leaderboardUI.SetActive(false);
+    }
+
+    public void ToggleLeaderboardUI()
+    {
+        if (leaderboardUI != null)
+        {
+            if (leaderboardUI.activeSelf)
+                leaderboardUI.SetActive(false);
+            else
+                leaderboardUI.SetActive(true);
+        }
+    }
+}
