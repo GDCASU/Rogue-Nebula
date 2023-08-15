24d8a6cdc002e7f2b5f41fddcb0d210aeeda6d12
Aaron Huggins <achuggi1@asu.edu>
2023-08-14 23:34:00 -0700

    Balance Changes
    Balance game some

--- Assets/Scripts/EnvObjects/RotateObject.cs
+++ Assets/Scripts/EnvObjects/RotateObject.cs
@@ -7,10 +7,17 @@ public class RotateObject : MonoBehaviour
     [SerializeField] float aroundXSpeed = 0f;
     [SerializeField] float aroundYSpeed = 0f;
     [SerializeField] float aroundZSpeed = 0f;
+    [SerializeField] float delay = 0f;
 
     // Update is called once per frame
     void Update()
     {
+        if(delay > 0f)
+        {
+            delay -= Time.deltaTime;
+            return;
+        }
+
         transform.Rotate(aroundXSpeed * Time.deltaTime, aroundYSpeed * Time.deltaTime, aroundZSpeed * Time.deltaTime);
     }
 }
