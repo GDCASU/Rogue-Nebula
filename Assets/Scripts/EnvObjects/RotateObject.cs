1c340a9b88f5989ebc6b98cbff61452413a49e40
Ryan Carpenter <dev.ryan1184@gmail.com>
2024-07-20 19:36:49 -0700

    Update RotateObject.cs

--- Assets/Scripts/EnvObjects/RotateObject.cs
+++ Assets/Scripts/EnvObjects/RotateObject.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
+    [Header("Rotation")]
     [SerializeField] float aroundXSpeed = 0f;
     [SerializeField] float aroundYSpeed = 0f;
     [SerializeField] float aroundZSpeed = 0f;
     [SerializeField] float delay = 0f;
+
+    [Header("Random Rotation")]
+    [SerializeField] bool randomRotation = false;
+    [SerializeField] float minAroundXSpeed = 0f, maxAroundXSpeed = 0f;
+    [SerializeField] float minAroundYSpeed = 0f, maxAroundYSpeed = 0f;
+    [SerializeField] float minAroundZSpeed = 0f, maxAroundZSpeed = 0f;
+
 
     // Update is called once per frame
     void Update()
@@ -18,6 +26,23 @@ public class RotateObject : MonoBehaviour
             return;
         }
 
+        if (randomRotation)
+            RandomRotation();
+        else
+            Rotate();
+    }
+
+    private void Rotate()
+    {
         transform.Rotate(aroundXSpeed * Time.deltaTime, aroundYSpeed * Time.deltaTime, aroundZSpeed * Time.deltaTime);
+    }
+
+    private void RandomRotation()
+    {
+        float randAroundXSpeed = Random.Range(minAroundXSpeed, maxAroundXSpeed);
+        float randAroundYSpeed = Random.Range(minAroundYSpeed, maxAroundYSpeed);
+        float randAroundZSpeed = Random.Range(minAroundZSpeed, maxAroundZSpeed);
+
+        transform.Rotate(randAroundXSpeed * Time.deltaTime, randAroundYSpeed * Time.deltaTime, randAroundZSpeed * Time.deltaTime);
     }
 }
